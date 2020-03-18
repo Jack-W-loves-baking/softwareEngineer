c7238045b536c19f628f544c9543e2e498337ca4
= <fkd2190@autuni.ac.nz>
2020-03-18 14:58:54 +1300

    written first test

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Example test', () =>{
+    it('this test should always pass', () =>{
+        expect(true.to.equal(true))
+    })
+})
